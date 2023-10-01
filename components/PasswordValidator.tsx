d1a89d546477a8f75ff3b57891ba99d5b1585a6a
NotNotRama <ramiroherrera.utn@gmail.com>
2023-10-01 16:27:45 -0300

    Added validation functions and rules

--- components/PasswordValidator.tsx
+++ components/PasswordValidator.tsx
@@ -1,12 +1,14 @@
 import { Box, Flex, Heading, Input } from '@chakra-ui/react';
 import { useForm } from 'react-hook-form';
 
-export type FormInput = {
+type FormInput = {
   input: string;
 };
 
+export type ValidationRules = Record<string, (value: string) => boolean>;
+
 export default function PasswordValidator({
-  requirements,
+  requirements = [],
 }: {
   requirements?: string[];
 }) {
@@ -14,6 +16,24 @@ export default function PasswordValidator({
     criteriaMode: 'all',
     mode: 'onChange',
   });
+  const inputValue = watch('input');
+
+  const validationRules: ValidationRules = {};
+
+  const validationFunctions: Record<string, (value: string) => boolean> = {
+    required: (value) => !!value,
+    digits: (value) => !!(value && /\d/.test(value)),
+    specialChars: (value) => !!(value && /[!@#$%^&*]/.test(value)),
+    uppercaseLetter: (value) => !!(value && /[A-Z]/.test(value)),
+    noConsecutiveLetters: (value) => !/([a-zA-Z])\1/i.test(value),
+  };
+
+  requirements.forEach((requirement) => {
+    if (validationFunctions[requirement]) {
+      validationRules[requirement] = validationFunctions[requirement];
+    }
+  });
+
   function handleKeyDown(e: React.KeyboardEvent<HTMLInputElement>) {
     if (e.key === 'Enter') {
       e.preventDefault();
